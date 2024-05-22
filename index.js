105ec9a1b80dbae3e99dd565a52e1da83383694b
MritulAich <mridulaich332@gmail.com>
2024-05-22 13:34:23 +0600

    env username and password had been decoded

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 //middleware
 app.use(cors());
 app.use(express.json());
-
 
 
 const uri = `mongodb+srv://${process.env.S3_BUCKET}:${process.env.SECRET_KEY}@cluster0.ku98crh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
