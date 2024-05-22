34b2516fa7cd4eb476b57692e3986b842ba1ef00
MritulAich <mridulaich332@gmail.com>
2024-05-22 15:44:05 +0600

    delete operation added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -46,7 +46,15 @@ async function run() {
     })
 
     
+    app.delete('/spot/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await spotCollection.deleteOne(query);
+      res.send(result);
+    })
 
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
